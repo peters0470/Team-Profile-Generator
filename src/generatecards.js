7d33328e06254a201d772763861a6396236595fc
unknown <peters0470@gmail.com>
2020-11-30 06:52:19 -0600

    added engineer and intern cards to html genterator

--- src/generatecards.js
+++ src/generatecards.js
@@ -25,6 +25,54 @@ const generateCards = (employees) => {
     `;
 };
 
+const createEngineer = (engineer) => {
+    return `
+            <div class="col s4">
+                <div class="card deep-orange accent-3">
+                    <div class="card content teal lighten-3 center-align">
+                        <h2 class="card-title">${engineer.getName()}</h2>
+                        <h5><span class="icon icons"><i class="fas fa-laptop-code"></i></span>${engineer.getRole()}</h5>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-content">
+                            <span>ID:${engineer.getId()}</span>
+                        </div>
+                        <div class="card-content">
+                            <span>Email:<a class='black-text' href='mailto:${engineer.getEmail()}'> ${engineer.getEmail()}</a></span>
+                        </div>
+                        <div class="card-content">
+                            <span>GitHub:<a href='https://github.com/${engineer.getGithub()}' target='_blank'> ${engineer.getGithub()}</a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+};
+
+const createIntern = (intern) => {
+    return `
+            <div class="col s4">
+                <div class="card deep-orange accent-3">
+                    <div class="card content teal lighten-3 center-align">
+                        <h2 class="card-title">${intern.getName()}</h2>
+                        <h5><span class="icon icons"><i class="fas fa-book"></i></span>${intern.getRole()}</h5>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-content">
+                            <span>ID:${intern.getId()}</span>
+                        </div>
+                        <div class="card-content">
+                            <span>Email:<a class='black-text' href='mailto:${intern.getEmail()}'> ${intern.getEmail()}</a></span>
+                        </div>
+                        <div class="card-content">
+                            <span>School: ${intern.getSchool()}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+};
+
     
 }
 module.exports = (employeeHtml) => {
